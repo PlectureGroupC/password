abd170ceb37ef8a9c7f52852d1ae06812c576877
yuppy-yamamo <14fi116@ms.dendai.ac.jp>
2017-04-14 03:24:36 +0900

    [add]ログインページの追加

---
+++ src/main/java/com/example/Entity/MemberEntity.java
@@ -0,0 +1,7 @@
+package com.example.Entity;
+
+/**
+ * Created by yuppy on 2017/04/14.
+ */
+public class MemberEntity {
+}
---
+++ src/main/java/com/example/web/LoginController.java
@@ -0,0 +1,7 @@
+package com.example.web;
+
+/**
+ * Created by yuppy on 2017/04/14.
+ */
+public class LoginController {
+}
