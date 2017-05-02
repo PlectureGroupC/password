f77f5031c6e597fcd1183c07b833ac6932e8f7e9
yuppy-yamamo <14fi116@ms.dendai.ac.jp>
2017-05-02 13:54:50 +0900

    [add]repositoryの追加

--- src/main/java/com/example/repository/WebInfoRepository.java
+++ src/main/java/com/example/repository/WebInfoRepository.java
@@ -1,6 +1,6 @@
-package com.example.domain.repository;
+package com.example.repository;
 
-import com.example.domain.model.WebInfo;
+import com.example.model.WebInfo;
 import org.springframework.data.repository.RepositoryDefinition;
 
 @RepositoryDefinition(domainClass = WebInfo.class, idClass = Integer.class)
