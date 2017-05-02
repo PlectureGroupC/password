664c6504428024a5d8172d5787a8977c0311b032
yuppy-yamamo <14fi116@ms.dendai.ac.jp>
2017-05-02 15:26:23 +0900

    [update]service追加に伴う変更

--- src/main/java/com/example/repository/WebInfoRepository.java
+++ src/main/java/com/example/repository/WebInfoRepository.java
@@ -1,10 +1,10 @@
 package com.example.repository;
 
 import com.example.model.WebInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.RepositoryDefinition;
 
-@RepositoryDefinition(domainClass = WebInfo.class, idClass = Integer.class)
-public interface WebInfoRepository {
-    WebInfo save(WebInfo info);
-    Iterable<WebInfo> findAll();
+public interface WebInfoRepository extends JpaRepository<WebInfo, Integer>{
+//    WebInfo save(WebInfo info);
+//    Iterable<WebInfo> findAll();
 }
