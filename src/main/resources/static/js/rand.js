a5331fea53f7b38a35decbe9f5313fe3821d5201
yuppy-yamamo <14fi116@ms.dendai.ac.jp>
2017-05-05 03:32:28 +0900

    [add]乱数生成する処理

---
+++ src/main/resources/static/js/rand.js
@@ -0,0 +1,9 @@
+function makeRandomstr() {
+    var length = document.getElementsById("length").value;
+    var str = array_merge(range('a', 'z'), range('A', 'Z'), range('0', '9'));
+    var r_str = '';
+    for($i = 0; $i < length; $i++){
+        r_str = str[rand(0, count(str) - 1)];
+    }
+    document.getElementById("output").innerHTML = r_str;
+}
