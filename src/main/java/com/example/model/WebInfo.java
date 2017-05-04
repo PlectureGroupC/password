ad2d6ba83bf09e6353bbf4c95b3e235a9dc02c18
yuppy-yamamo <14fi116@ms.dendai.ac.jp>
2017-05-04 18:37:55 +0900

    [update]主キーとそれを自動生成する処理の追加

--- src/main/java/com/example/model/WebInfo.java
+++ src/main/java/com/example/model/WebInfo.java
@@ -6,11 +6,23 @@ import javax.persistence.*;
 @Table(name = "web_info")
 public class WebInfo {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "number")
+    private int number;
+
+    @Column(name = "mail_address")
     private String mailaddress;
+
+    @Column(name = "web_name")
     private String name;
+
+    @Column(name = "web_url")
     private String url;
+
+    @Column(name = "web_userid")
     private String userID;
+
+    @Column(name = "web_password")
     private String password;
 
     public WebInfo(){
