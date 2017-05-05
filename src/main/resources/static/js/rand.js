370373b773995a8a8ad1f28b584b64f9ba548694
yuppy-yamamo <14fi116@ms.dendai.ac.jp>
2017-05-06 00:28:03 +0900

    [add]bootstrapの導入

--- src/main/resources/static/js/rand.js
+++ src/main/resources/static/js/rand.js
@@ -1,9 +1,9 @@
 function makeRandomstr() {
-    var length = document.getElementsById("length").value;
+    var length = document.getElementById('length').value;
     var str = array_merge(range('a', 'z'), range('A', 'Z'), range('0', '9'));
     var r_str = '';
     for($i = 0; $i < length; $i++){
         r_str = str[rand(0, count(str) - 1)];
     }
-    document.getElementById("output").innerHTML = r_str;
+    document.getElementById('output').innerHTML = r_str;
 }
