e9582c2bd59da809e621ae96cb1565f41fe5c8db
yuppy-yamamo <14fi116@ms.dendai.ac.jp>
2017-05-22 12:25:02 +0900

    [update]ImgCertの反映

--- src/main/java/com/example/controller/AccountController.java
+++ src/main/java/com/example/controller/AccountController.java
@@ -4,6 +4,7 @@ import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import com.example.domain.service.ImageService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -23,23 +24,65 @@ import com.example.form.AccountForm;
 public class AccountController {
 	@Autowired
 	AccountService accountService;
+
+	@Autowired
+	ImageService imageService;
 	
 	@ModelAttribute
 	AccountForm setUpForm() {
 		return new AccountForm();
 	}
+
+	@ModelAttribute("checkItems")
+	private Map<String, String> getCheckItems(){
+		final Map<String, String> CHECK_ITEMS =
+				Collections.unmodifiableMap(new LinkedHashMap<String, String>() {
+					private static final long serialVersionUID = 178167752967848875L;
+					{
+						put("/images/img1.png", "1");
+						put("/images/img2.png", "2");
+						put("/images/img3.png", "3");
+						put("/images/img4.png", "4");
+						put("/images/img5.png", "5");
+						put("/images/img6.png", "6");
+						put("/images/img7.png", "7");
+						put("/images/img8.png", "8");
+						put("/images/img9.png", "9");
+					}
+				});
+		return CHECK_ITEMS;
+	}
 	
 	@RequestMapping(value="create", method=RequestMethod.POST)
+	//account生成時に呼び出す
 	String create(@Validated AccountForm form, BindingResult result, Model model) {
 		if(result.hasErrors()) {
 			return "/account/create";
 		}
 		if(!form.getMailAddress().equals(form.getConfirmMail())){
-			model.addAttribute("mailError", true);
-			model.addAttribute("mailErrorMessage", "MailとMail(確認用)の入力が一致しません");
+			model.addAttribute("error", true);
+			model.addAttribute("errorMessage", "MailとMail(確認用)の入力が一致しません");
+			return "/account/create";
+		}
+		else if(!form.getPassword().equals(form.getConfirmPass())){
+			model.addAttribute("error", true);
+			model.addAttribute("errorMessage","PasswordとPassword(確認用)の入力が一致しません");
+			return "/account/create";
+		}
+		else if(form.getImgPath().size() != 3){
+			model.addAttribute("error", true);
+			model.addAttribute("errorMessage", "Password復元時に使用する画像を3枚選択してください");
 			return "/account/create";
 		}
 		Account account = new Account();
+		String hashSeed = "";
+		for(String path: form.getImgPath()){
+			//Image image = imageService.findImageByImgName(path);
+			//hashSeed = hashSeed + image.getSeed();
+			System.out.println(path);
+			hashSeed = hashSeed + imageService.findImgSeedByImgName(path);
+			//hashSeed = hashSeed + path;
+		}
 		BeanUtils.copyProperties(form, account);
 		accountService.create(account);
 		return "redirect:/index";
@@ -50,34 +93,4 @@ public class AccountController {
 		model.addAttribute("form", new AccountForm());
 		return "/account/create";
 	}
-	
-	@RequestMapping(value="forgotPass", method=RequestMethod.GET)
-	String forgotPass(Model model) {
-		model.addAttribute("checkItems", CHECK_ITEMS);
-		return "/account/forgotPassword";
-	}
-	
-	@RequestMapping(value="forgotPass", method=RequestMethod.POST)
-	String forgotPass(@ModelAttribute AccountForm form, BindingResult result, Model model) {
-		if(result.hasErrors()) {
-			return "/account/forgotPass";
-		}
-		return "";
-	}
-	
-	final static Map<String, String> CHECK_ITEMS =
-		    Collections.unmodifiableMap(new LinkedHashMap<String, String>() {
-				private static final long serialVersionUID = 178167752967848875L;
-				{
-					put("/images/img1.png", "1");
-					put("/images/img2.png", "2");
-					put("/images/img3.png", "3");
-					put("/images/img4.png", "4");
-					put("/images/img5.png", "5");
-					put("/images/img6.png", "6");
-					put("/images/img7.png", "7");
-					put("/images/img8.png", "8");
-					put("/images/img9.png", "9");
-				}
-		    });
 }
