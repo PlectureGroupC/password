f8705eab9b12a3b0ea8e8c155bfbdd8ecdbb12ee
yuppy-yamamo <14fi116@ms.dendai.ac.jp>
2017-05-22 13:17:10 +0900

    [update]methodの変更

--- src/main/java/com/example/PassApplication.java
+++ src/main/java/com/example/PassApplication.java
@@ -1,14 +1,11 @@
 package com.example;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PassApplication {
-	public static void main(String[] args) throws FileNotFoundException, IOException {
+	public static void main(String[] args){
 		SpringApplication.run(PassApplication.class, args);
 	}
 }
