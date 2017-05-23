9a7d84f6d9127f1d2a0913c42204ed8db50899c1
kidoshin <kszkshine@gmail.com>
2017-05-24 04:10:16 +0900

    viewとPassControllerの変更

--- src/main/java/com/example/form/ResetPassForm.java
+++ src/main/java/com/example/form/ResetPassForm.java
@@ -1,15 +1,18 @@
 package com.example.form;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 
 @Data
 public class ResetPassForm {
 	@NotNull
+	@Size(min=12)
 	private String password;
 
 	@NotNull
+	@Size(min=12)
 	private String confirmPass;
 
 	@NotNull
