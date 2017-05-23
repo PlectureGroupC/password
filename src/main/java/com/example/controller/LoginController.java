46709964bf7d478fadb0728bbe14cb9b95ed9726
yuppy-yamamo <14fi116@ms.dendai.ac.jp>
2017-05-23 12:26:43 +0900

    [update]値送信のための処理の変更

--- src/main/java/com/example/controller/LoginController.java
+++ src/main/java/com/example/controller/LoginController.java
@@ -1,7 +1,6 @@
 package com.example.controller;
 
 import com.example.domain.model.Account;
-import com.example.domain.service.AccountUserDetails;
 import com.example.repository.LoginRepository;
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,12 +8,8 @@ import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.form.*;
